009206d8cc0c2f8b54c1b52149f8a3a2fa1f20c1
andela-cdike <chukwuerika.dike@andela.com>
2017-12-02 08:39:20 +0100

    Expose API endpoint

---
+++ testxblock/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+
+from .views import TestView
+
+
+app_name = 'gk_api_testxblock'
+
+
+urlpatterns = [
+    url(r'^testxblock$', TestView.as_view(), name='test_view'),
+]
