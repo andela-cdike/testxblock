726278cd7d21d401795ee4a930291e6350bdeff5
andela-cdike <chukwuerika.dike@andela.com>
2017-12-03 19:32:38 +0100

    Switch renderer classes to browsable API

--- testxblock/views.py
+++ testxblock/views.py
@@ -1,11 +1,14 @@
 import re
 import requests
 
-from rest_framework.views import APIView
+from rest_framework.renderers import BrowsableAPIRenderer
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 class QuoteView(APIView):
+
+    renderer_classes = (BrowsableAPIRenderer,)
 
     def get(self, request, format=None):
         """Returns quotes from a third party service"""
@@ -15,22 +18,10 @@ class QuoteView(APIView):
         )
         resp = requests.get(api_endpoint)
         if resp.status_code == 200:
-            quotes = [strip_html_tags(quote['content'])
-                      for quote in resp.json()]
+            quotes = [quote['content'].replace('\n', '') for quote in resp.json()]
             data = {
                 'msg': 'Welcome to my test endpoint. Enjoy these quotes.',
                 'quotes': quotes
             }
             return Response(data, status=200)
         return Response('Oops, something went wrong! Try again', status=500)
-
-
-def strip_html_tags(text):
-    """Strips leading and trailing HTML tags from supplied text
-    and returns cleaned text
-
-    :param text: string
-    :returns: string
-    """
-    tag_pattern = re.compile(r'<[^>]+>')
-    return tag_pattern.sub('', text)
