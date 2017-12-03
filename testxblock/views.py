cfa95696fc9961cd43058c6c0d3f3f31601ac073
andela-cdike <chukwuerika.dike@andela.com>
2017-12-03 19:20:47 +0100

    Update view to return quotes

--- testxblock/views.py
+++ testxblock/views.py
@@ -1,3 +1,4 @@
+import re
 import requests
 
 from rest_framework.views import APIView
@@ -14,6 +15,22 @@ class QuoteView(APIView):
         )
         resp = requests.get(api_endpoint)
         if resp.status_code == 200:
-            quotes = [quote['content'] for quote in resp.json()]
-            return Response(quotes, status=200)
+            quotes = [strip_html_tags(quote['content'])
+                      for quote in resp.json()]
+            data = {
+                'msg': 'Welcome to my test endpoint. Enjoy these quotes.',
+                'quotes': quotes
+            }
+            return Response(data, status=200)
         return Response('Oops, something went wrong! Try again', status=500)
+
+
+def strip_html_tags(text):
+    """Strips leading and trailing HTML tags from supplied text
+    and returns cleaned text
+
+    :param text: string
+    :returns: string
+    """
+    tag_pattern = re.compile(r'<[^>]+>')
+    return tag_pattern.sub('', text)
